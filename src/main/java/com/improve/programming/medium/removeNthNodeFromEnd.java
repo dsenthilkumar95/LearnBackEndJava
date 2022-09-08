345d89d3296ace3438e170b879a8972b996a49f5
dsenthilkumar95 <dsenthilkumar95@gmail.com>
2022-09-08 22:03:32 +0530

    Adding code to delete nth node from end

--- src/main/java/com/improve/programming/medium/removeNthNodeFromEnd.java
+++ src/main/java/com/improve/programming/medium/removeNthNodeFromEnd.java
@@ -9,15 +9,43 @@ public class removeNthNodeFromEnd {
             end = end.next;
             n--;
         }
-        while(end != null) {
+        while(end.next != null) {
             prev = start;
             start = start.next;
             end = end.next;
         }
         if(prev == null) {
-
+            head = head.next;
+        } else {
+            prev.next = start.next;
         }
         return head;
+    }
+
+//    Efficient solution is move till n nodes from start, if its end return head.next, else more one more step and
+//    iterate till the end, then remove keep start.next = start.next.next and return head
+
+    public ListNode removeNthFromEndEfficientSubmissions(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        int count = 0;
+        while(dummy != null && dummy.next != null) {
+            count++;
+            dummy = dummy.next;
+        }
+        int num = count - n;
+
+        ListNode dummy2 = new ListNode();
+        dummy2.next = head;
+        ListNode p = dummy2;
+
+        int curr = 0;
+        while(p != null && p.next != null && curr<num) {
+            p = p.next;
+            curr++;
+        }
+        p.next = p.next.next;
+        return dummy2.next;
     }
 
 }

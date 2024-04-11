1dd04b8476139c2f41facb8cef16cd71688e3a1c
dsenthilkumar95 <dsenthilkumar95@gmail.com>
2024-04-11 13:40:57 +0530

    assigning child to parent array

---
+++ src/main/java/com/improve/javaBasics/InherArray.java
@@ -0,0 +1,9 @@
+package com.improve.javaBasics;
+
+public class InherArray {
+    public static void main(String[] args) {
+        Child[] childArray = new Child[10];
+        Parent[] parentArray = new Child[10];
+        parentArray[5] = new Parent();
+    }
+}
